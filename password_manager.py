1b48124ff4a3a8c117f10ccce7a537295fe99308
Lucas David Barrera Mejia <lucasbame@unisabana.edu.co>
2026-04-09 11:40:42 -0500

    password_manager.py

--- password_manager.py
+++ password_manager.py
@@ -5,7 +5,11 @@ from caesar import caesar_encrypt
 
 def encrypt_single_pass(filename: str) -> None:
     """TODO: Parte 1."""
-    pass
+    with open(filename, "r") as archivo:
+        contraseña = archivo.read().strip()
+    contraseña_encriptada = caesar_encrypt(contraseña)
+    with open(filename, "w") as archivo:
+        archivo.write(contraseña_encriptada)
 
 
 def encrypt_passwords_in_file(filename: str) -> None:
@@ -21,3 +25,6 @@ def change_password(filename: str, website: str, password: str) -> bool:
 def add_login(filename: str, website_name: str, username: str, password: str) -> None:
     """TODO: Parte 4."""
     pass
+
+
+encrypt_single_pass("examples/example1.txt")
